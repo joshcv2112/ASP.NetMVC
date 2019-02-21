d0f7a930afe64e9cf6b63745cceb02c379d4bf93
joshcv2112 <joshcv2112@gmail.com>
2019-02-20 22:46:56 -0700

    More routing stuff

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -17,6 +17,11 @@ namespace Vidly.Controllers
             return View(movie);
         }
 
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content($"{year}/{month}");
+        }
+
         public ActionResult Edit(int id)
         {
             return Content("id=" + id);
